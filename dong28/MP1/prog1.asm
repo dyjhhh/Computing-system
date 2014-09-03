2d7e56a67052feb619e277dc052839e5cecb573f
dong28 <dong28@5648d728-c28d-4c1b-9d7e-5884e9d3ff1d>
2014-09-03 16:05:59 +0000

    My submission is done

--- dong28/MP1/prog1.asm
+++ dong28/MP1/prog1.asm
@@ -85,14 +85,14 @@
 	
 	; fill the histogram with zeroes 
 	AND R6,R6,#0		; put a zero into R6
-	LD R1,NUM_BINS		; initialize loop count to 27
+	LD  R1,NUM_BINS		; initialize loop count to 27
 	ADD R2,R0,#0		; copy start of histogram into R2
 
 	; loop to fill histogram starts here
 HFLOOP	STR R6,R2,#0		; write a zero into histogram
 	ADD R2,R2,#1		; point to next histogram entry
 	ADD R1,R1,#-1		; decrement loop count
-	BRp HFLOOP		; continue until loop count reaches zero
+	BRp HFLOOP		    ; continue until loop count reaches zero
 
 	; initialize R1, R3, R4, and R5 from memory
 	LD R3,NEG_AT		; set R3 to additive inverse of ASCII '@'
@@ -130,18 +130,71 @@ MORE_THAN_Z
 	ADD R2,R2,R5		; subtract '`' - '@' from the character
 	BRnz NON_ALPHA		; if <= '`', i.e., < 'a', go increment non-alpha
 	ADD R6,R2,R4		; compare with 'z'
-	BRnz ALPHA		; if <= 'z', go increment alpha count
+	BRnz ALPHA		    ; if <= 'z', go increment alpha count
 	BRnzp NON_ALPHA		; otherwise, go increment non-alpha
 
 GET_NEXT
 	ADD R1,R1,#1		; point to next character in string
 	BRnzp COUNTLOOP		; go to start of counting loop
 
-
-
 PRINT_HIST
+    LD R0,POS_AT        ; load POS_AT into r0.
+    OUT                 ; print ASCII character '@' onto screen
+    LD R0, ASC_SPACE    ; load ASC_SPACE into R0
+    OUT                 ; print a space onto screen
+    LD R1,POS_AT        ; load POS_AT into R1
+    LD R2,HIST_ADDR     ; load histogram starting address into R2
 
-; you will need to insert your code to print the histogram here
+PRINT_HEX
+	LDR R3,R2,#0
+    AND R5,R5,#0        ; initialize digit count to 0
+    
+DIG_LOOP
+    AND R4,R4,#0        ; initialize bit count to 0
+    AND R0,R0,#0        ; initialize current digit to 0
+    
+BIT_LOOP
+    ADD R0,R0,R0        ; double the current digit (shift left)
+    ADD R3,R3,#0        ; check if it is the MSB set
+    BRzp MSB_CLEAR
+    ADD R0,R0,#1        ; if so, add 1 to digit
+    
+MSB_CLEAR
+    ADD R3,R3,R3        ; get rid of that bit (shift left)
+    ADD R4,R4,#1        ; increment the bit count
+    ADD R6,R4,#-4       ; check if we have four bits yet 
+    BRn BIT_LOOP        ; if not, go get another
+    ADD R6,R0,#-10      ; is the digit >= 10?
+    BRzp HIGH_DIGIT     ; if so, we need to print a letter
+    LD R6,ASC_ZERO      ; add ’0’ to digits < 10
+    BRnzp PRINT_DIGIT
+    
+HIGH_DIGIT
+    LD R6,ASC_HIGH      ; add ’A’ - 10 to digits >= 10
+PRINT_DIGIT
+    ADD R0,R0,R6        ; calculate the digit
+    OUT
+    ADD R5,R5,#1        ; increment the digit count
+    ADD R6,R5,#-4       ; check if printed both digits yet?
+    BRn DIG_LOOP        ; if not, go print another
+
+    LEA R0, NEWLN       ; Load address of newln into r0.
+    LDR R0,R0,#0        ; Load contents into r0.
+    OUT                 ; Print R0 (i.e. a newline).
+
+    ADD R1,R1, #1       ; go to next char in ASCII table.
+    ADD R0,R1, #0       ; store content of R1 into R0
+    LD  R6,NEG_Z        ; load the additive inverse of ASCII 'Z'
+    ADD R6,R6,R1        ; compare the value with 'Z'
+    BRzp DONE           ; if the character is 'Z', go to done
+    ADD R0,R1,#0        ; store content of R1 into R0
+    OUT                 ; print out the ascii character
+    LD  R0,ASC_SPACE    ; load R0 with ASC_SPACE
+    OUT                 ; print out a space
+    ADD R2,R2,#1        ; go to the next histogram entry
+    LD R6, NEG_Z        ; load the additive inverse of ASCII 'Z'
+    ADD R6,R6,R1        ; compare with 'Z'
+    BRn PRINT_HEX       ; Loop if the character is not 'Z' yet 
 
 
 
@@ -153,8 +206,14 @@ NUM_BINS	.FILL #27	; 27 loop iterations
 NEG_AT		.FILL xFFC0	; the additive inverse of ASCII '@'
 AT_MIN_Z	.FILL xFFE6	; the difference between ASCII '@' and 'Z'
 AT_MIN_BQ	.FILL xFFE0	; the difference between ASCII '@' and '`'
-HIST_ADDR	.FILL x3F00     ; histogram starting address
+HIST_ADDR	.FILL x3F00 ; histogram starting address
 STR_START	.FILL x4000	; string starting address
+ASC_HIGH    .FILL X0037
+ASC_ZERO    .FILL X0030
+POS_AT      .FILL x0040
+NEWLN       .stringz "\n" 
+ASC_SPACE   .FILL x0020
+NEG_Z       .FILL xFFA5
 
 ; for testing, you can use the lines below to include the string in this
 ; program...
